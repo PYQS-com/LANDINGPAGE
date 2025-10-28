7ee55eaf54a9a2eebb60b07c234d904b643f3ab3
Athul Nambiar <dev.athulnambiar@gmail.com>
2025-10-28 23:15:10 +0530

    Hide the reviews - build fixes

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import Community from "./components/Community.vue";
 import Contact from "./components/Contact.vue";
 import FAQ from "./components/FAQ.vue";
 import Footer from "./components/Footer.vue";
-import ReviewsMarquee from '@/components/ReviewsMarquee.vue';
+// import ReviewsMarquee from '@/components/ReviewsMarquee.vue';
 // import Header from "./components/Header.vue";
 import SmoothCursor from "./components/magicui/SmoothCursor.vue";
 </script>
